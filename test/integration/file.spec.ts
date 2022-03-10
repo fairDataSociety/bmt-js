966a3c11abccc4734b787c9ee4038da48b576a2f
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2022-03-10 16:38:09 +0100

    test: integration test on files

---
+++ test/integration/file.spec.ts
@@ -0,0 +1,46 @@
+import { Bee, SPAN_SIZE } from '@ethersphere/bee-js'
+import FS from 'fs'
+import path from 'path'
+import { DEFAULT_MAX_PAYLOAD_SIZE, makeChunkedFile } from '../../src'
+import { bytesToHex } from '../../src/utils'
+
+const beeUrl = process.env.BEE_API_URL || 'http://localhost:1633'
+const bee = new Bee(beeUrl)
+const stamp = process.env.BEE_POSTAGE
+
+describe('file', () => {
+  it('should produce same BMT tree like Bee for data > 4KB', async () => {
+    const fileBytes = Uint8Array.from(
+      FS.readFileSync(path.join(__dirname, '..', 'test-files', 'The-Book-of-Swarm.pdf')),
+    )
+    const chunkedFile = makeChunkedFile(fileBytes)
+    const result = await bee.uploadData(stamp, fileBytes)
+    const reference = result.reference
+
+    const spanAndChunkPayloadLength = DEFAULT_MAX_PAYLOAD_SIZE + SPAN_SIZE
+    const beeRootChunk = await bee.downloadChunk(reference)
+    expect(beeRootChunk.length).toBe(968)
+    const bee2ndLayer1stChunkAddress = beeRootChunk.slice(8, 40)
+    const bee2ndLayer1stChunk = await bee.downloadChunk(bytesToHex(bee2ndLayer1stChunkAddress, 64))
+    expect(bee2ndLayer1stChunk.length).toBe(spanAndChunkPayloadLength)
+    const beeLeafLayer1stChunk = await bee.downloadChunk(bytesToHex(bee2ndLayer1stChunk.slice(8, 40), 64))
+    expect(beeLeafLayer1stChunk.length).toBe(spanAndChunkPayloadLength)
+
+    const tree = chunkedFile.bmtTree()
+    expect(tree[0][0].payload).toStrictEqual(beeLeafLayer1stChunk.slice(8))
+
+    expect(tree[1][0].payload).toStrictEqual(bee2ndLayer1stChunk.slice(8))
+    expect(tree[1][0].span()).toStrictEqual(bee2ndLayer1stChunk.slice(0, 8))
+    expect(tree[1][0].address()).toStrictEqual(bee2ndLayer1stChunkAddress)
+    expect(bytesToHex(tree[2][0].address(), 64)).toStrictEqual(reference)
+  })
+
+  it('should produce same chunk like Bee for data < 4KB', async () => {
+    const fileBytes = Uint8Array.from(FS.readFileSync(path.join(__dirname, '..', 'test-files', 'text.txt')))
+    const chunkedFile = makeChunkedFile(fileBytes)
+    const result = await bee.uploadData(stamp, fileBytes)
+    const reference = result.reference
+
+    expect(bytesToHex(chunkedFile.address(), 64)).toStrictEqual(reference)
+  })
+})
