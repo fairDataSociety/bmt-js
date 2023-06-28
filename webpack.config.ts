e15544d3330ab2c1f347ab81a998208b30969f0f
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-06-28 11:16:24 +0200

    build: use sourcemap that works

--- webpack.config.ts
+++ webpack.config.ts
@@ -27,12 +27,12 @@ const base = async (env?: Partial<WebpackEnvParams>): Promise<Configuration> => 
   return {
     bail: Boolean(isProduction),
     mode: env?.mode || 'development',
-    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+    devtool: 'eval-source-map',
     entry,
     output: {
       path,
       filename,
-      sourceMapFilename: filename + '.map',
+      // sourceMapFilename: filename + '.map',
       library: 'BmtJs',
       libraryTarget: 'umd',
       globalObject: 'this',
@@ -80,7 +80,7 @@ const base = async (env?: Partial<WebpackEnvParams>): Promise<Configuration> => 
               ecma: 5,
               comments: false,
             },
-            sourceMap: true,
+            // sourceMap: true,
           },
           // Use multi-process parallel running to improve the build speed
           // Default number of concurrent runs: os.cpus().length - 1
